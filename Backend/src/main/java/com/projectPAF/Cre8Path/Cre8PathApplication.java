4d3101ffd6f8a6f92c12549b3601dfce8016b505
Devangap <101203575+Devangap@users.noreply.github.com>
2025-03-24 13:29:53 +0530

    Initial commit - Intializing frontend and backend

---
+++ Backend/src/main/java/com/projectPAF/Cre8Path/Cre8PathApplication.java
@@ -0,0 +1,13 @@
+package com.projectPAF.Cre8Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cre8PathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cre8PathApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/projectPAF/Cre8Path/Cre8PathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectPAF.Cre8Path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cre8PathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
